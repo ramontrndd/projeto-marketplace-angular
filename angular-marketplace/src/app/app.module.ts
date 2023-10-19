6fafc9f8c97be7685da421d98f325c125c5a3c71
unknown <ramonbraintrindade@gmail.com>
2023-10-19 20:25:04 -0300

    feat: add header and footer

--- angular-marketplace/src/app/app.module.ts
+++ angular-marketplace/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from "@angular/material/toolbar";
-import { HeaderComponent } from './components/template/header/header.component'
+import { HeaderComponent } from './components/template/header/header.component';
+import { FooterComponent } from './components/template/footer/footer.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
