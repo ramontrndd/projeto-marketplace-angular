e82325cfaf56529e0663de547f84b86da75faed9
Ramon Trindade <ramonbraintrindade@gmail.com>
2023-10-22 15:13:01 -0300

    feat: add select button

--- angular-marketplace/src/app/app.module.ts
+++ angular-marketplace/src/app/app.module.ts
@@ -8,12 +8,15 @@ import { HeaderComponent } from './components/template/header/header.component';
 import { FooterComponent } from './components/template/footer/footer.component'
 import  { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { NavbarComponent } from './components/template/navbar/navbar.component';
 import {MatCardModule} from '@angular/material/card';
 import { HomeComponent } from './views/home/home.component';
 import {HttpClientModule} from "@angular/common/http";
 import { CardFilmComponent } from './components/template/card-film/card-film.component';
 import { ListFilmsComponent } from './views/list-films/list-films.component';
+import { SelectButtonComponent } from './components/template/select-button/select-button.component';
 
 
 
@@ -26,6 +29,7 @@ import { ListFilmsComponent } from './views/list-films/list-films.component';
     HomeComponent,
     CardFilmComponent,
     ListFilmsComponent,
+    SelectButtonComponent,
     
   ],
   imports: [
@@ -36,7 +40,9 @@ import { ListFilmsComponent } from './views/list-films/list-films.component';
     MatListModule,
     MatSidenavModule,
     MatCardModule,
-    HttpClientModule
+    HttpClientModule,
+    MatButtonModule,
+    MatButtonToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular-marketplace/src/app/views/checkout/checkout.service.ts
+++ angular-marketplace/src/app/views/checkout/checkout.service.ts
@@ -4,18 +4,38 @@ import { Observable } from 'rxjs';
 import { Film } from '../home/list-films/film.model';
 
 
+
 @Injectable({
   providedIn: 'root'
 })
 export class CheckoutService {
   public baseUrl: string = 'http://localhost:3001/';
   public listFilms: Film[] = [];
+  public totalPrice: number = 0;
+  private _priceHandler: number = 0;
+
+  getPrice(): number{
+   return this._priceHandler;
+  }
+  setPrice(value: number){
+    this._priceHandler = value;
+  }
 
 
   constructor(private httpClient: HttpClient) { }
 
     getListFilms(): Observable<Film[]> {
       return this.httpClient.get<Film[]>(this.baseUrl+'films');
+    }
+
+    selectFilm(){this.totalPrice += this.getPrice();
+    }
+    
+    unselectFilm(){
+      this.totalPrice -= this.getPrice();
+      if (this.totalPrice < 0) {
+        this.totalPrice = 0;
+      }
     }
   }
 
