1ee1202443cd8e5bcda8be91c13f0282355de5f3
Ramon Trindade <ramonbraintrindade@gmail.com>
2024-04-13 01:45:20 -0300

    prod

--- src/app/shared/menuitem.ts
+++
@@ -1,5 +0,0 @@
-export type MenuItem = {
-    icon: string;
-    label: string;
-    route: string;
-  };
--- src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'https://films-api-v0tr.onrender.com'
-};
